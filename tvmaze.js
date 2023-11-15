b17c93308e53089d212b052aeb6d89cf1b0e4ba8
Carl Molina <carlandrew.molina@gmail.com>
2023-11-15 15:13:19 -0800

    Before code review

--- tvmaze.js
+++ tvmaze.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const TVMAZE_BASE_URL = "http://api.tvmaze.com/";
+const missingImg= `https://tinyurl.com/tv-missing`;
 
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
@@ -15,7 +16,6 @@ const $searchForm = $("#searchForm");
  */
 
 async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
 
   const searchTerm = term;
 
@@ -39,44 +39,22 @@ async function getShowsByTerm(term) {
 
   console.log("This is showData=", showData);
 
- // const {id,name,summary,image} = showData[0].show;
-  //console.log(name,summary); // sweet
-
   let result = [];
-  for(let show of showData){
-    //need to adjust for shows without image
-    if(!show.show.image){
-      var {id,name,summary, image = null} = show.show;
+  for(let entries of showData) {
+
+    if(!entries.show.image){
+      var {id,name,summary, image = null} = entries.show;
     }else{
-      var {id,name,summary,image} = show.show;
+      var {id,name,summary,image} = entries.show;
     }
 
-   show = {id,name,summary,image};
-   show.image = image ? image.medium : null;
-   result.push(show);
+   entries = {id,name,summary,image};
+   entries.image = image ? image.medium : missingImg;
+   result.push(entries);
   }
   console.log('the result',result);
   return result;
 
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
 }
 
 
@@ -93,7 +71,7 @@ function displayShows(shows) {
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src="${show.image}"
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
